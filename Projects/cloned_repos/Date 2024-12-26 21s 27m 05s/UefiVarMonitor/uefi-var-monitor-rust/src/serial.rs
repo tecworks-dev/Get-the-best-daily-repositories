b6d023f3800268dc1652b3991ca38e717172e6d7
amirroox <>
2024-12-26 21:37:19 +0000

    Update and clone latest repositories at 2024-12-26 21:37:19.043189105

---
+++ Projects/cloned_repos/Date 2024-12-26 21s 27m 05s/UefiVarMonitor/uefi-var-monitor-rust/src/serial.rs
@@ -0,0 +1,57 @@
+// uefi-var-monitor/src/serial.rs
+// Copyright © 2019 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Inspired by https://github.com/phil-opp/blog_os/blob/post-03/src/vga_buffer.rs
+// from Philipp Oppermann
+
+use core::fmt;
+use atomic_refcell::AtomicRefCell;
+use x86_64::instructions::port::PortWriteOnly;
+
+// We use COM1 as it is the standard first serial port.
+static PORT: AtomicRefCell<PortWriteOnly<u8>> = AtomicRefCell::new(PortWriteOnly::new(0x3f8));
+
+pub struct Serial;
+
+impl fmt::Write for Serial {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        let mut port = PORT.borrow_mut();
+        for b in s.bytes() {
+            unsafe { port.write(b) }
+        }
+        Ok(())
+    }
+}
+
+// New function to log error messages with a specific prefix
+pub fn log_error(message: &str) {
+    let _ = writeln!(Serial, "[ERROR] {}", message);
+}
+
+// New function to log info messages with a specific prefix
+pub fn log_info(message: &str) {
+    let _ = writeln!(Serial, "[INFO] {}", message);
+}
+
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {{
+        use core::fmt::Write;
+        #[cfg(all(feature = "log-serial", not(test)))]
+        writeln!(crate::serial::Serial, $($arg)*).unwrap();
+        #[cfg(all(feature = "log-serial", test))]
+        println!($($arg)*);
+    }};
+}
